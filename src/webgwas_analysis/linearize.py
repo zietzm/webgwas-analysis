047e6858f885f620dd59a609d9637032afa70904
zietzm <michael.zietz@gmail.com>
2024-12-05 20:08:15 -0800

    feat: add overall eval_df options to linearization

--- src/webgwas_analysis/linearize.py
+++ src/webgwas_analysis/linearize.py
@@ -15,15 +15,25 @@ def _approximate(
     reg.fit(X_df, Y_df)
     if eval_df is None:
         eval_df = df
+    else:
+        if not eval_df.columns == df.columns:
+            raise ValueError("df and eval_df must have the same columns")
+
     Yhat = reg.predict(eval_df.select(exogs).to_pandas())
     schema = [endog] if isinstance(endog, str) else endog
     return pl.DataFrame(Yhat, schema=schema)
 
 
 def approximate_all(
-    df: pl.DataFrame, endogs: list[str], exogs: list[str]
+    df: pl.DataFrame,
+    endogs: list[str],
+    exogs: list[str],
+    eval_df: pl.DataFrame | None = None,
 ) -> pl.DataFrame:
     """Approximate all phenotypes in the dataframe using linear regression."""
+
+    if eval_df is not None and not df.columns == eval_df.columns:
+        raise ValueError("df and eval_df must have the same columns")
 
     # Check for missingness
     phenotypes_with_missingness = (
@@ -34,22 +44,28 @@ def approximate_all(
         .to_list()
     )
     if len(phenotypes_with_missingness) > 0:
-        return approximate_all_with_missingness(df, endogs, exogs)
+        return approximate_all_with_missingness(df, endogs, exogs, eval_df)
 
-    return _approximate(df, endogs, exogs, None)
+    return _approximate(df, endogs, exogs, eval_df)
 
 
 def approximate_all_with_missingness(
-    df: pl.DataFrame, endogs: list[str], exogs: list[str]
+    df: pl.DataFrame,
+    endogs: list[str],
+    exogs: list[str],
+    eval_df: pl.DataFrame | None = None,
 ) -> pl.DataFrame:
     """Approximate all phenotypes in the dataframe using linear regression when
     missingness is present. Note, this is considerably slower than `approximate_all`.
     """
 
+    if eval_df is not None and not df.columns == eval_df.columns:
+        raise ValueError("df and eval_df must have the same columns")
+
     approx_df = dict()
     for endog in tqdm.tqdm(endogs):
         this_df = df.select([endog] + exogs).drop_nulls()
-        Yhat_df = _approximate(this_df, endog, exogs, df)
+        Yhat_df = _approximate(this_df, endog, exogs, eval_df)
         assert Yhat_df.shape[1] == 1
         approx_df[endog] = Yhat_df[endog]
 
